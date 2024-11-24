c1825e230370343befd17afceffe09be46e1a390
Gokul <117854447+go19kul16@users.noreply.github.com>
2024-11-25 01:23:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const Book = mongoose.model('Book', bookSchema);
 
 app.use(cors(
   {
-      origin:[],
+      origin:["book-management-zeta.vercel.app"],
       methods:["POST","GET"],
       credentials:true
   }
